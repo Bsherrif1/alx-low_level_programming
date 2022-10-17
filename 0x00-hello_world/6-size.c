c313e9e6908aa0e2d83f5dfec96abc2557f9e072
Babalola Aderemi Sherrifdeen <Bsherrifdeen@gmail.com>
2022-10-16 22:11:50 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
- * @void: parameter
- * Return: 0 (Success)
- */
+* main -Entry point
+* @void: parameter
+* Return: 0 (Success)
+*/
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -13,4 +13,4 @@ int main(void)
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-}	
+}
