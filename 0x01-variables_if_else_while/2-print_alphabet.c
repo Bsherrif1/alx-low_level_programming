c0cb3a3585284cfae16dab2b6b29e94146d5d11c
Babalola Aderemi Sherrifdeen <Bsherrifdeen@gmail.com>
2022-10-17 10:35:16 -0700

    combine

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		putchar(le);
+	putchar('\n');
+	return (0);
